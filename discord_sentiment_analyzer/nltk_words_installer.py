e77fd0050b4cfd7ca464e5caf675674bc6fa3294
Ice <70650330+meteometeo@users.noreply.github.com>
2022-01-02 19:41:18 +0900

    Add files via upload

---
+++ discord_sentiment_analyzer/nltk_words_installer.py
@@ -0,0 +1,14 @@
+# You can install nltk words using this installer
+
+import nltk
+
+nltk.download([
+    "names",
+    "stopwords",
+    "state_union",
+    "twitter_samples",
+    "movie_reviews",
+    "averaged_perceptron_tagger",
+    "vader_lexicon",
+    "punkt",
+])
